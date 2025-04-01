80a2857fddd817151adfdc17ab7c9e9e3bee24af
sofro21 <andreisofronie25@gmail.com>
2025-04-01 11:53:59 +0200

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    distDir: 'out',
-    images: {
-      unoptimized: true
-    }
-  };
-  
-  module.exports = nextConfig;
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/cost-of-equity-app' : '',
+  assetPrefix: isProd ? '/cost-of-equity-app/' : '',
+};
