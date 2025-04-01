489ca30faf686cbfae57dbcb2a30df0a639a2859
sofro21 <andreisofronie25@gmail.com>
2025-04-01 11:19:22 +0200

    allow_frontend_gloval

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Allow frontend on localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
